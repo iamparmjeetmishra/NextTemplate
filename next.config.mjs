dc16c11450124eb3b17bd57cf10e6be3045cfe95
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-20 20:42:28 +0530

    NextTemplate V1

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+import createJiti from "jiti";
+import { fileURLToPath } from "node:url";
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+jiti("./src/env/server.ts")
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+   experimental: {
+      typedRoutes: true
+   }
+};
+
+export default nextConfig;
---
+++ src/app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "App Title",
+  description: "App Description",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎯</text></svg>"
+      />
+
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} h-screen w-screen antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
